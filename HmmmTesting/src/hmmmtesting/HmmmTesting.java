1726c4d164e7a660d5939422ec5a1389b8a9b21e
Hugo Mateo <hmateo@emergya.com>
2018-03-01 10:11:10 +0100

    Here come the puppies!

---
+++ HmmmTesting/src/hmmmtesting/HmmmTesting.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hmmmtesting;
+
+/**
+ *
+ * @author hmateo
+ */
+public class HmmmTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Who let the dogs out?");
+        for(int i=0; i<4;i++){
+            System.out.println("Woof!");
+        }
+    }
+    
+}
